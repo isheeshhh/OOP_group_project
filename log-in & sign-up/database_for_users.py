bea3fbf51af91977ed6b64f059bdcc1cb08c3af1
kvzcrz <kivzeimerclaorcruz@gmail.com>
2026-06-09 21:11:28 +0800

    Add files via upload

---
+++ log-in & sign-up/database_for_users.py
@@ -0,0 +1,109 @@
+import json
+import os
+
+DATABASE_FILE = "users.json"
+
+
+
+def initialize_database():
+    """
+    Creates users.json if it does not exist.
+    """
+
+    if not os.path.exists(DATABASE_FILE):
+
+        data = {
+            "users": []
+        }
+
+        with open(DATABASE_FILE, "w") as file:
+            json.dump(data, file, indent=4)
+
+
+def load_users():
+    with open(DATABASE_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_users(data):
+    """
+    Saves updated data to users.json.
+    """
+
+    with open(DATABASE_FILE, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def username_exists(username):
+    """
+    Checks if a username already exists.
+    """
+
+    data = load_users()
+
+    for user in data["users"]:
+
+        if user["username"] == username:
+            return True
+
+    return False
+
+
+def register_user(fullname, username, password, role):
+    """
+    Creates a new account.
+    """
+
+    if username_exists(username):
+        return False
+
+    data = load_users()
+
+    new_user = {
+        "fullname": fullname,
+        "username": username,
+        "password": password,
+        "role": role
+    }
+
+    data["users"].append(new_user)
+
+    save_users(data)
+
+    return True
+
+
+def login_user(username, password):
+    """
+    Verifies login credentials.
+    """
+
+    data = load_users()
+
+    for user in data["users"]:
+
+        if (
+            user["username"] == username and
+            user["password"] == password
+        ):
+            return user
+
+    return None
+
+
+def get_user(username):
+    """
+    Returns a user record.
+    """
+
+    data = load_users()
+
+    for user in data["users"]:
+
+        if user["username"] == username:
+            return user
+
+    return None
+
+
+initialize_database()
