98658a6b7928fc815c55d864eb3cbb38acda98fd
Irish De Guzman <irishdeguzman.acads@gmail.com>
2026-06-12 00:02:22 +0800

    edit

--- course_management/course.py
+++ course_management/course.py
@@ -1,26 +1,34 @@
 import os
+import shutil
 import json
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 COURSES_DATABASE = os.path.join(BASE_DIR, "courses.json")
+FILE_DIR = os.path.join(BASE_DIR, "file_storage")
 
-def load_courses():
-    try:
-        with open(COURSES_DATABASE, "r") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []  
+def init_directory():
+    if not os.path.exists(FILE_DIR):
+        os.makedirs(FILE_DIR)
 
-def save_courses(data):
-    with open(COURSES_DATABASE, "w") as f:
+def load_data(filepath):
+    if not os.path.exists(filepath):
+        with open(filepath, 'w') as f:
+            json.dump([], f)
+        return []
+
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+def save_data(filepath, data):
+    with open(filepath, "w") as f:
         return json.dump(data, f, indent=4)
 
 def create_course(course_name, course_description):
     if not course_name.strip():
         raise ValueError("Course Name must not be left blank.")
     
-    courses = load_courses()
+    courses = load_data(COURSES_DATABASE)
     for c in courses:
         if c['course_name'] == course_name.strip():
             raise ValueError(f"Course: {course_name} already exists.")
@@ -31,4 +39,65 @@ def create_course(course_name, course_description):
         "course_files" : [] 
     }
     courses.append(new_course)
-    save_courses(courses)
+    save_data(COURSES_DATABASE, courses)
+
+def add_file_to_storage(local_path):
+    init_directory()
+    file_name = os.path.basename(local_path)
+    destination = os.path.join(FILE_DIR, file_name)
+    shutil.copy(local_path, destination)
+    return file_name
+
+def delete_file_in_storage(local_path):
+    if local_path:
+        file_name = os.path.join(FILE_DIR, local_path)
+        if os.path.exists(file_name):
+            try:
+                os.remove(file_name)
+            except OSError:
+                pass
+
+def add_file_to_course(course, local_file_path):
+    target_course = course.strip()
+
+    courses = load_data(COURSES_DATABASE)
+    course_found = False
+
+    new_file = os.path.basename(local_file_path)
+
+    for c in courses:
+        if c['course_name'] == target_course:
+            for existing_file in c['course_files']:
+                if os.path.basename(existing_file) == new_file:
+                    raise ValueError(f"The file '{new_file}' is already uploaded.")
+                
+            saved_path = add_file_to_storage(local_file_path)
+            c['course_files'].append(saved_path)
+            course_found = True
+            break
+    
+    if not course_found:
+        raise ValueError (f"Course '{target_course}' does not exist. ")
+    
+    save_data(COURSES_DATABASE, courses)
+
+def delete_file_in_course(course, local_file_path):
+    target_course = course.strip()
+
+    courses = load_data(COURSES_DATABASE)
+    course_found = False
+
+    for c in courses:
+        if c['course_name'] == target_course:
+            if local_file_path in c['course_files']:
+                delete_file_in_storage(local_file_path)
+                c['course_files'].remove(local_file_path)
+                course_found = True
+                break
+    
+    if not course_found:
+        raise ValueError("File or course could not be found.")
+    
+    save_data(COURSES_DATABASE, courses)
+
+
