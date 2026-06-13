d84a1ea49f4d7ac00db065d10e1dff5a1e7c8d57
kvzcrz <kivzeimerclaorcruz@gmail.com>
2026-06-13 20:07:33 +0800

    updated sign up and login files, no students homepage yet

---
+++ log-in & sign-up/email_config.py
@@ -0,0 +1,10 @@
+﻿# Email address that will send OTP messages.
+SMTP_EMAIL = "studio.kzcc@gmail.com"
+
+# App password generated from the sender email account.
+SMTP_APP_PASSWORD = "wcyp fwlr phmo echq"
+
+# Gmail SMTP server settings.
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
+    
